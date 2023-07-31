3e2d44950aeff95fac8390a0cc998c1f7e9e8bf6
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-07-31 14:52:56 -0300

    config get rentals route

--- src/routers/rentals.routes.js
+++ src/routers/rentals.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { addRental } from "../controllers/rentals.controller.js";
+import { addRental, getRental } from "../controllers/rentals.controller.js";
 import { validateSchema } from "../middlewares/validateSchema.js"
 import { rentalSchema } from "../schemas/rentals.schema.js";
 
 const rentalsRouter = Router();
+rentalsRouter.get("/rentals", getRental);
 rentalsRouter.post("/rentals", validateSchema(rentalSchema), addRental);
 export default rentalsRouter;
