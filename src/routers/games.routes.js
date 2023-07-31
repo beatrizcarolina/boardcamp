617f3cec98128303a94f71e113990a1b210cce16
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-07-30 23:44:43 -0300

    create addGames function

--- src/routers/games.routes.js
+++ src/routers/games.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { getGames } from "../controllers/games.controller.js";
+import { addGames, getGames } from "../controllers/games.controller.js";
+import { validateSchema } from "../middlewares/validateSchema.js";
+import { gameSchema } from "../schemas/games.schema.js";
 
 const gamesRouter = Router();
 gamesRouter.get("/games", getGames);
-
+gamesRouter.post("/games", validateSchema(gameSchema), addGames);
 
 export default gamesRouter;
