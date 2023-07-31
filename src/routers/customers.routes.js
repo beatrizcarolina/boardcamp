33bd7eb0a2172e6085fcb697ace27b87b28aba3e
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-07-31 00:26:13 -0300

    add getCustomersByID route

--- src/routers/customers.routes.js
+++ src/routers/customers.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { validateSchema } from "../middlewares/validateSchema.js"
-import { addCustomer, getCustomers } from "../controllers/customers.controller.js";
+import { addCustomer, getCustomers, getCustomersByID } from "../controllers/customers.controller.js";
 import { customerSchema } from "../schemas/customers.schema.js";
 
 const customersRouter = Router();
 customersRouter.get("/customers", getCustomers);
+customersRouter.get("/customers/:id", getCustomersByID);
 customersRouter.post("/customers", validateSchema(customerSchema), addCustomer);
 
 export default customersRouter;
