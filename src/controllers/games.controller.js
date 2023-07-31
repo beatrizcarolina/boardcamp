e36dbb12a944732d0e99dd29401a75773a0de6e2
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-07-30 23:43:46 -0300

    implement addGames function

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -11,3 +11,24 @@ export async function getGames(req,res) {
         return res.status(500).send(error.message);
     }
 };
+
+export async function addGames(req,res) {
+    const { name, image, stockTotal, pricePerDay } = req.body;
+
+    try {
+        const foundGame = await db.query(`SELECT "name" FROM games WHERE "name"=$1`, [name]);
+        if (foundGame.rowCount !== 0) {
+            return res.sendStatus(409);
+        }
+
+        await db.query(`INSERT INTO games ("name", "image", "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4);`, [
+            name,
+            image,
+            stockTotal,
+            pricePerDay,
+        ]);
+        return res.sendStatus(201);        
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+};
